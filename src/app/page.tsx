b43be72aaf2cf9498322832de866a7f3e1dbddf9
Wesley <wesmith314@gmail.com>
2024-03-01 18:19:15 -0800

    removes starting information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,15 +22,7 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        
-          
-        
-       
-
-        
-
-
-      </div>
+     </div>
     </main>
   );
 }
