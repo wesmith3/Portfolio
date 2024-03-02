e22fa1919cf8e9439daa0af5cc259aeabe56ca12
Wesley <wesmith314@gmail.com>
2024-03-01 18:18:10 -0800

    removes docs title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,12 +28,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
+          
         
         </a>
 
