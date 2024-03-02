c505e54831de7640bac586aeb4c567567aefe45c
Wesley <wesmith314@gmail.com>
2024-03-01 18:16:35 -0800

    removes learn description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,9 +51,6 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
         </a>
 
 
