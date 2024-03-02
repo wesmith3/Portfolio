1d94c2dd71b95777e06b52674e930980b53cb09e
Wesley <wesmith314@gmail.com>
2024-03-01 18:10:22 -0800

    removes deploy title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,12 +79,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
+          
         </a>
       </div>
     </main>
