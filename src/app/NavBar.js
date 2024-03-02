68ddbd45e3b97821f78174536b1dc96ffdee7651
Wesley <wesmith314@gmail.com>
2024-03-02 15:07:10 -0800

    adds menu-btn pics

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -1,10 +1,52 @@
 import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import Home from '../public/Home.jpeg'
+import Home from '../public/Home.jpeg'
+import Home from '../public/Home.jpeg'
 
 function NavBar() {
   return (
-    <div>
-      
-    </div>
+    <nav className='navbar'>
+      <ul>
+        <li>
+          <Link href="/">
+            <Image
+            src={Home}
+            className="menu-btn"
+            alt="Next.js Logo"
+            width={100}
+            height={50}
+            priority
+          />
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+          <Image
+            src={About}
+            className="menu-btn"
+            alt="Next.js Logo"
+            width={100}
+            height={50}
+            priority
+          />
+          </Link>
+        </li>
+        <li>
+          <Link href="/portfolio">
+          <Image
+            src={Portfolio}
+            className="menu-btn"
+            alt="Next.js Logo"
+            width={100}
+            height={50}
+            priority
+          />
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
