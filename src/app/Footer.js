f711738108ceec7b8a0a0f363a4d334ae16a380c
Wesley <wesmith314@gmail.com>
2024-03-02 20:51:48 -0800

    formats footer

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,16 +1,20 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faMedium,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
-    <div className='footer'>
-    <div className="brand-icons">
-                    <FontAwesomeIcon className="li-icon" icon={faLinkedin}/>
-                    <FontAwesomeIcon className="gh-icon" icon={faGithub} />
-                    <FontAwesomeIcon className="m-icon" icon={faMedium}/>
-     </div>
+    <div className="footer">
+      <div className="brand-icons">
+        <FontAwesomeIcon className="li-icon" icon={faLinkedin} />
+        <FontAwesomeIcon className="gh-icon" icon={faGithub} />
+        <FontAwesomeIcon className="m-icon" icon={faMedium} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
