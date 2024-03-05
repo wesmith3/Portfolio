a08feda5f2ba1b7b36cc042e2a3c3466d1187d44
Wesley <wesmith314@gmail.com>
2024-03-05 08:36:04 -0800

    changes proj descriptions

--- src/app/projects/projects.tsx
+++ src/app/projects/projects.tsx
@@ -3,7 +3,7 @@ import AA from "./AA.jpeg";
 import FB from "./FB.jpeg";
 import Vignette from "./Vignette.jpeg";
 import Link from "next/link";
-import GitHubIcon from '@mui/icons-material/GitHub';
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function Projects() {
   return (
@@ -19,14 +19,14 @@ export default function Projects() {
           height={800}
         />
         <h1 className="description">
-          An innovative art gallery social media platform,
-          enabling users to curate their own virtual art galleries, share their
-          creations, explore, engage with, and follow fellow artists works,
-          fostering a vibrant online art community.
+          An innovative art gallery social media platform, enabling users to
+          curate their own virtual art galleries, share their creations,
+          explore, engage with, and follow fellow artists works, fostering a
+          vibrant online art community.
         </h1>
-        <Link legacyBehavior href="https://github.com/wesmith3">
+        <Link legacyBehavior href="https://github.com/wesmith3/Vignette">
           <a className="gh-icon" target="_blank">
-            <GitHubIcon fontSize="large"></GitHubIcon>
+            <GitHubIcon fontSize="large" />
           </a>
         </Link>
       </div>
@@ -40,6 +40,19 @@ export default function Projects() {
           width={400}
           height={800}
         />
+        <h1 className="description">
+          Step into the virtual aisles of our Blockbuster-inspired website,
+          where you can register, peruse an extensive movie selection,
+          rent/return your favorites, share your thoughts through reviews.
+        </h1>
+        <Link
+          legacyBehavior
+          href="https://github.com/wesmith3/Flatbuster-Video"
+        >
+          <a className="gh-icon" target="_blank">
+            <GitHubIcon fontSize="large" />
+          </a>
+        </Link>
       </div>
       <div className="proj">
         <h2 className="proj-name">Audio Abyss</h2>
@@ -51,6 +64,18 @@ export default function Projects() {
           width={800}
           height={800}
         />
+        <h1 className="description">
+          Explore endless music possibilities with our app&apos;s diverse song
+          library. Discover new favorites through randomized song selections,
+          where you can listen, add to your playlist, and rate with a simple
+          like or dislike—an essential tool for music lovers craving fresh
+          beats.
+        </h1>
+        <Link legacyBehavior href="https://github.com/zachtalmadge/audio-abyss">
+          <a className="gh-icon" target="_blank">
+            <GitHubIcon fontSize="large" />
+          </a>
+        </Link>
       </div>
     </div>
   );
