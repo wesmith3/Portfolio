ff6f8d8f5cfef9fe0fdc2b63b436437e33255b06
Wesley <wesmith314@gmail.com>
2024-03-05 12:32:25 -0800

    adds embed resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function page() {
+export default function Page() {
   return (
-    <div className='resume'>
-      <embed src='https://docs.google.com/document/d/1VFcmElqITGICJ5PJJUPsTMoWoUUcKeR0pIJrISRFoQs/edit' type="application/pdf" width="80%" height="750vh" />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '105vh' }}>
+      {/* <embed className='resume' src='https://docs.google.com/document/d/e/2PACX-1vToL9A8kw_RkO5Hil5Ook14D8zT0Ck6ybcj1GMhU5TeFAYzrE2DXmGw7eQQ1pTeDNeC5sGcDPmGWfNI/pub' type="application/pdf" width="90%" height="750vh" /> */}
+      <iframe src="https://docs.google.com/document/d/e/2PACX-1vToL9A8kw_RkO5Hil5Ook14D8zT0Ck6ybcj1GMhU5TeFAYzrE2DXmGw7eQQ1pTeDNeC5sGcDPmGWfNI/pub?embedded=true" height='70%'width='75%'></iframe>
     </div>
-  )
+  );
 }
