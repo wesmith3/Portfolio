f7fda0bce91d943e72916f6bb2d80b8588e639a7
Wesley <wesmith314@gmail.com>
2024-03-04 17:34:38 -0800

    adds new font

--- src/app/projects/projects.tsx
+++ src/app/projects/projects.tsx
@@ -1,23 +1,50 @@
-import Image from 'next/image'
-import AA from './AA.jpeg'
-import FB from './FB.jpeg'
-import Vignette from './Vignette.png'
+import Image from "next/image";
+import AA from "./AA.jpeg";
+import FB from "./FB.jpeg";
+import Vignette from "./Vignette.jpeg";
 
 export default function Projects() {
   return (
-    <div className='proj-list'>
-        <div className='proj'>
-        <h1 className='proj-name'>Vignette</h1>
-        <Image src={Vignette} alt='Vignette' className='vignette-img' layout='responsive' width={800} height={800}/>
-        </div>
-        <div className='proj'>
-        <h1 className='proj-name'>Flatbuster Video</h1>
-        <Image src={FB} alt='FlatbusterVideo' className='fb-img' layout='responsive' width={800} height={800}/>
-        </div>
-        <div className='proj'>
-        <h1 className='proj-name'>Audio Abyss</h1>
-        <Image src={AA} alt='AudioAbyss' className='aa-img' layout='responsive' width={800} height={800}/>
-        </div>
+    <div className="proj-list">
+      <div className="proj">
+        <h2 className="proj-name">Vignette</h2>
+        <Image
+          src={Vignette}
+          alt="Vignette"
+          className="vignette-img"
+          layout="responsive"
+          width={800}
+          height={800}
+        />
+        <p className="description">
+          An innovative art gallery social media platform akin to Instagram,
+          enabling users to curate their own virtual art galleries, share their
+          creations, explore, engage with, and follow fellow artists works,
+          fostering a vibrant online art community.
+        </p>
+      </div>
+      <div className="proj">
+        <h2 className="proj-name">Flatbuster Video</h2>
+        <Image
+          src={FB}
+          alt="FlatbusterVideo"
+          className="fb-img"
+          layout="responsive"
+          width={400}
+          height={800}
+        />
+      </div>
+      <div className="proj">
+        <h2 className="proj-name">Audio Abyss</h2>
+        <Image
+          src={AA}
+          alt="AudioAbyss"
+          className="aa-img"
+          layout="responsive"
+          width={800}
+          height={800}
+        />
+      </div>
     </div>
-  )
+  );
 }
