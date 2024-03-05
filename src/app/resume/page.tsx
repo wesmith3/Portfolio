d8f8c0e9b56a15a5677ec3485d8fb4492a90b1d3
Wesley <wesmith314@gmail.com>
2024-03-05 11:48:15 -0800

    changes width of resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      <embed src='https://docs.google.com/document/d/1VFcmElqITGICJ5PJJUPsTMoWoUUcKeR0pIJrISRFoQs/edit' type="application/pdf" width="100%" height="600px" />
+    <div className='resume'>
+      <embed src='https://docs.google.com/document/d/1VFcmElqITGICJ5PJJUPsTMoWoUUcKeR0pIJrISRFoQs/edit' type="application/pdf" width="80%" height="750vh" />
     </div>
   )
 }
