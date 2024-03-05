4a3360ee5fd71b9384b7103d86c5902c3a4b3656
Wesley <wesmith314@gmail.com>
2024-03-05 13:33:41 -0800

    fixes css issues

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import './page.css'
 
 export default function Page() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '105vh' }}>
+    <div>
+      <h1 className='resume-title'>Resume</h1>
+    <div className='container'>
       {/* <embed className='resume' src='https://docs.google.com/document/d/e/2PACX-1vToL9A8kw_RkO5Hil5Ook14D8zT0Ck6ybcj1GMhU5TeFAYzrE2DXmGw7eQQ1pTeDNeC5sGcDPmGWfNI/pub' type="application/pdf" width="90%" height="750vh" /> */}
-      <iframe src="https://docs.google.com/document/d/e/2PACX-1vToL9A8kw_RkO5Hil5Ook14D8zT0Ck6ybcj1GMhU5TeFAYzrE2DXmGw7eQQ1pTeDNeC5sGcDPmGWfNI/pub?embedded=true" height='70%'width='75%'></iframe>
+      <iframe src="https://docs.google.com/document/d/e/2PACX-1vToL9A8kw_RkO5Hil5Ook14D8zT0Ck6ybcj1GMhU5TeFAYzrE2DXmGw7eQQ1pTeDNeC5sGcDPmGWfNI/pub?embedded=true" height='80%'width='75%'></iframe>
+    </div>
     </div>
   );
 }
