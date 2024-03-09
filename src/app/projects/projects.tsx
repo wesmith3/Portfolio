ef1438ae7f8686b6402ded325b08e1ec4f46d7d6
Wesley <wesmith314@gmail.com>
2024-03-08 23:22:15 -0800

    formats projects

--- src/app/projects/projects.tsx
+++ src/app/projects/projects.tsx
@@ -40,7 +40,7 @@
 //       <Card sx={{ maxWidth: 400 }}>
 //         <CardMedia
 //           component="div"
-//           sx={{ height: 300 }} 
+//           sx={{ height: 300 }}
 //           title="Flatbuster Video"
 //         >
 //           <Image src={FB} alt="Flatbuster-Video" title="Flatbuster Video" width={450} />
@@ -65,7 +65,7 @@
 //       <Card sx={{ maxWidth: 400 }}>
 //         <CardMedia
 //           component="div"
-//           sx={{ height: 300 }} 
+//           sx={{ height: 300 }}
 //           title="Audio Abyss"
 //         >
 //           <Image src={AA} alt="Audio Abyss" title="Audio Abyss" width={450} />
@@ -180,12 +180,13 @@ export default function Projects() {
       <div className="card-container">
         {/* Card 1 */}
         <Card sx={{ maxWidth: 450, marginRight: 2 }} className="proj">
-          <CardMedia
-            component="div"
-            sx={{ height: 300 }}
-            title="Vignette"
-          >
-            <Typography gutterBottom variant="h5" component="div" className="proj-name">
+          <CardMedia component="div" sx={{ height: 300 }} title="Vignette">
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="proj-name"
+            >
               Vignette
             </Typography>
             <Image src={Vignette} alt="Vignette" title="Vignette" width={400} />
@@ -199,14 +200,11 @@ export default function Projects() {
             </Typography>
           </CardContent>
           <CardActions>
-          <Link
-          legacyBehavior
-          href="https://github.com/wesmith3/Vignette"
-          >
-          <a className="github" target="_blank">
-            <GitHubIcon fontSize="large" />
-          </a>
-        </Link>
+            <Link legacyBehavior href="https://github.com/wesmith3/Vignette">
+              <a className="github" target="_blank">
+                <GitHubIcon fontSize="large" />
+              </a>
+            </Link>
           </CardActions>
         </Card>
 
@@ -215,58 +213,72 @@ export default function Projects() {
           <CardMedia
             component="div"
             sx={{ height: 300 }}
-            title="Flatbuster Video">
-            <Typography gutterBottom variant="h5" component="div" className="proj-name">
+            title="Flatbuster Video"
+          >
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="proj-name"
+            >
               Flatbuster Video
             </Typography>
-            <Image src={FB} alt="Flatbuster-Video" title="Flatbuster Video" width={400} />
+            <Image
+              src={FB}
+              alt="Flatbuster-Video"
+              title="Flatbuster Video"
+              width={400}
+            />
           </CardMedia>
           <CardContent>
             <Typography variant="body2" color="white">
-            Step into the virtual aisles of our Blockbuster-inspired website,
-           where you can register, peruse an extensive movie selection,rent/return your favorites, share your thoughts through reviews.
+              Step into the virtual aisles of our Blockbuster-inspired website,
+              where you can register, peruse an extensive movie
+              selection,rent/return your favorites, share your thoughts through
+              reviews.
             </Typography>
           </CardContent>
           <CardActions>
-          <Link
-          legacyBehavior
-          href="https://github.com/wesmith3/Flatbuster-Video"
-          >
-          <a className="github" target="_blank">
-            <GitHubIcon fontSize="large" />
-          </a>
-        </Link>
+            <Link
+              legacyBehavior
+              href="https://github.com/wesmith3/Flatbuster-Video"
+            >
+              <a className="github" target="_blank">
+                <GitHubIcon fontSize="large" />
+              </a>
+            </Link>
           </CardActions>
         </Card>
 
         {/* Card 3 */}
         <Card sx={{ maxWidth: 400 }} className="proj">
-          <CardMedia
-            component="div"
-            sx={{ height: 300 }}
-            title="Audio Abyss">
-            <Typography gutterBottom variant="h5" component="div" className="proj-name">
+          <CardMedia component="div" sx={{ height: 300 }} title="Audio Abyss">
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="proj-name"
+            >
               Audio Abyss
             </Typography>
             <Image src={AA} alt="Audio Abyss" title="Audio Abyss" width={400} />
           </CardMedia>
           <CardContent>
             <Typography variant="body2" color="white">
-            Discover new favorites through randomized song selections,
-             where you can listen, add to your playlist, and rate with a simple
-             like or dislike—an essential tool for music lovers craving fresh
-             beats.
+              Discover new favorites through randomized song selections, where
+              you can listen, add to your playlist, and rate with a simple like
+              or dislike—an essential tool for music lovers craving fresh beats.
             </Typography>
           </CardContent>
           <CardActions>
-          <Link
-          legacyBehavior
-          href="https://github.com/zachtalmadge/audio-abyss"
-          >
-          <a className="github" target="_blank">
-            <GitHubIcon fontSize="large" />
-          </a>
-        </Link>
+            <Link
+              legacyBehavior
+              href="https://github.com/zachtalmadge/audio-abyss"
+            >
+              <a className="github" target="_blank">
+                <GitHubIcon fontSize="large" />
+              </a>
+            </Link>
           </CardActions>
         </Card>
       </div>
